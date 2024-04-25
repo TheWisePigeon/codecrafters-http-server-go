337a725ff5b5a70c2ce6f9d3786310fe6a5d9afa
TheWisePigeon <zozozozeph@gmail.com>
2024-04-25 21:13:06 +0000

    stage 6

--- app/server.go
+++ app/server.go
@@ -26,22 +26,9 @@ func parseHeaders(content []string) HeaderMap {
 	return headers
 }
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func handleConnection(conn net.Conn) {
 	data := make([]byte, 1024)
-	_, err = conn.Read(data)
+	_, err := conn.Read(data)
 	if err != nil {
 		fmt.Println("Error reading from connection: ", err.Error())
 		os.Exit(1)
@@ -86,5 +73,22 @@ func main() {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
 		}
+	}
+}
+
+func main() {
+	fmt.Println("Logs from your program will appear here!")
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
 }
