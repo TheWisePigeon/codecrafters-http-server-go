34c714dfae0f2a42ff2b6ce388370d4532a4d7de
TheWisePigeon <zozozozeph@gmail.com>
2024-04-26 13:35:40 +0000

    refactor code

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -18,8 +19,16 @@ const (
 
 type HeaderMap map[string]string
 
-func parseHeaders(content []string) HeaderMap {
-	headers := HeaderMap{}
+type HTTPRequest struct {
+	Method      string
+	Path        string
+	HTTPVersion string
+	Headers     map[string]string
+	Body        []byte
+}
+
+func parseHeaders(content []string) map[string]string {
+	headers := map[string]string{}
 	for _, line := range content {
 		if line == "" {
 			break
@@ -30,24 +39,38 @@ func parseHeaders(content []string) HeaderMap {
 	return headers
 }
 
+func parseHTTPRequest(content []byte) *HTTPRequest {
+	req := &HTTPRequest{}
+	byteParts := bytes.Split(content, []byte("\r\n\r\n"))
+	req.Body = byteParts[1]
+	parts := strings.Split(string(byteParts[0]), "\r\n")
+	startLineParts := strings.Split(parts[0], " ")
+	req.Method = startLineParts[0]
+	req.Path = startLineParts[1]
+	req.HTTPVersion = startLineParts[2]
+	req.Headers = parseHeaders(parts[1:])
+	return req
+}
+
 func handleConnection(conn net.Conn) {
+	defer conn.Close()
 	data := make([]byte, 1024)
 	_, err := conn.Read(data)
 	if err != nil {
 		fmt.Println("Error reading from connection: ", err.Error())
 		os.Exit(1)
 	}
-	parts := strings.Split(string(data), "\r\n")
-	startLine := parts[0]
-	path := strings.Split(startLine, " ")[1]
-	if path == "/" {
+	req := parseHTTPRequest(data)
+	if req.Path == "/" {
 		_, err := conn.Write([]byte(Response200))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
 		}
-	} else if strings.HasPrefix(path, "/echo") {
-		randomStr := strings.TrimPrefix(path, "/echo/")
+		return
+	}
+	if strings.HasPrefix(req.Path, "/echo") {
+		randomStr := strings.TrimPrefix(req.Path, "/echo/")
 		response := "HTTP/1.1 200 OK\r\n"
 		response += "Content-Type: text/plain\r\n"
 		response += fmt.Sprintf("Content-Length: %d\r\n", len(randomStr))
@@ -56,11 +79,11 @@ func handleConnection(conn net.Conn) {
 		_, err := conn.Write([]byte(response))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
 		}
-	} else if path == "/user-agent" {
-		headers := parseHeaders(parts[1:])
-		userAgent := headers["user-agent"]
+		return
+	}
+	if req.Path == "/user-agent" {
+		userAgent := req.Headers["user-agent"]
 		response := "HTTP/1.1 200 OK\r\n"
 		response += "Content-Type: text/plain\r\n"
 		response += fmt.Sprintf("Content-Length: %d\r\n", len(userAgent))
@@ -69,10 +92,11 @@ func handleConnection(conn net.Conn) {
 		_, err := conn.Write([]byte(response))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
 		}
-	} else if strings.HasPrefix(path, "/files") {
-		fileName := strings.TrimPrefix(path, "/files/")
+		return
+	}
+	if strings.HasPrefix(req.Path, "/files") {
+		fileName := strings.TrimPrefix(req.Path, "/files/")
 		filePath := fmt.Sprintf("%s/%s", dir, fileName)
 		file, err := os.Open(filePath)
 		if err != nil {
@@ -80,17 +104,16 @@ func handleConnection(conn net.Conn) {
 				_, err = conn.Write([]byte(Response404))
 				if err != nil {
 					fmt.Println("Error writing to client: ", err.Error())
-					os.Exit(1)
 				}
-				os.Exit(0)
+				return
 			}
 			fmt.Println("Error opening file for reading: ", err.Error())
-			os.Exit(1)
+			return
 		}
 		fileContent, err := io.ReadAll(file)
 		if err != nil {
 			fmt.Println("Error reading from file: ", err.Error())
-			os.Exit(1)
+			return
 		}
 		response := "HTTP/1.1 200 OK\r\n"
 		response += "Content-Type: application/octet-stream\r\n"
@@ -99,14 +122,13 @@ func handleConnection(conn net.Conn) {
 		_, err = conn.Write([]byte(response))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
 		}
-	} else {
-		_, err = conn.Write([]byte(Response404))
-		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
-		}
+		return
+	}
+	_, err = conn.Write([]byte(Response404))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		return
 	}
 }
 
@@ -124,7 +146,7 @@ func main() {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			return
 		}
 		go handleConnection(conn)
 	}
